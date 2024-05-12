975777ba4fe55dba38cd204e524491019a6acfbc
mstrrich <marstonrichjhon@gmail.com>
2024-05-12 10:01:49 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+
+
+from src.MotionModel import moving_robot
+
+''' Compare performance according to the number of   '''
+
+''' 20 obstacle'''
+print('Moiving robot with 20 obstacle: ')
+moving_robot(N_obstacle=20, iteration=50)
+print('finished')
+
+''' 20 obstacle'''
+print('Moiving robot with 20 obstacle ')
+moving_robot(N_obstacle=20, iteration=50)
+print('finished')
+
+print('Moiving robot with 20 obstacle: ')
+moving_robot(N_obstacle=20, iteration=50)
+print('finished')
+
+''' 10 obstacle and '''
+print('Moiving robot with 10 obstacle')
+moving_robot(N_obstacle=10, iteration=50)
+print('finished')
+
+''' 20 obstacle'''
+print(' with 20  : ')
+moving_robot(N_obstacle=20, iteration=50)
+print('finished')
+
+''' 40 obstacle'''
+print('Moiving robot with 40 obstacle and 500  : ')
+moving_robot(N_obstacle=40, iteration=50)
+print('finished')
+
+
+
